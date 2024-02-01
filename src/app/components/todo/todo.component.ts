bef026b31a865f8487442354d6c41056b292da11
mitulpatel224 <mitul.patel224@gmail.com>
2024-02-01 18:33:03 +0530

    docs: descriptive comments for signals

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,7 +1,17 @@
-import { Component, computed, signal } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  computed,
+  effect,
+  signal,
+} from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule, NgForm, NgModelGroup } from '@angular/forms';
 
+/**
+ * Type: Todo Item 
+ */
 type TodoItem = {
   id: number;
   task: string;
@@ -16,16 +26,32 @@ type TodoItem = {
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent {
-  /** Signal<TodoItem[]>: Todo list */
+export class TodoComponent implements OnDestroy {
+  /**
+   * Signal<TodoItem[]>: Todo list
+   */
   public todoList = signal<TodoItem[]>([
     { id: 1, status: true, task: 'Placeholder task...' },
   ]);
 
-  /** Computed total pending items */
+  /**
+   * Computed total pending items
+   * computed() executes its callback function each time when there is any changes in a signal used inside it!
+   */
   public totalPendingItems = computed(() =>
     this.todoList().filter((task) => task.status === false)
   );
+
+  /**
+   * Effects to trigger any side-effect due to a signal
+   * effect() executes its callback function each time when there is any changes in a signal used inside it!
+   */
+  private logger = effect(() => {
+    console.log(
+      'Pending todos:',
+      this.todoList().filter((task) => task.status === false)
+    );
+  });
 
   /**
    * Add new task to the Todo list
@@ -38,6 +64,7 @@ export class TodoComponent {
     if (!valid) return;
 
     const { task } = value;
+    // signal.update() creates a new instance (different reference) of value
     this.todoList.update((list) => [
       ...list,
       { id: list.length + 1, status: false, task },
@@ -51,6 +78,12 @@ export class TodoComponent {
    * @param task TodoItem
    */
   public taskChecked(index: number, task: TodoItem): void {
+    // signal.mutate() changes the existing instance (same reference) of value
     this.todoList.mutate((old) => (old[index] = task));
+  }
+
+  ngOnDestroy(): void {
+    // Destroys logger effect
+    this.logger.destroy();
   }
 }
